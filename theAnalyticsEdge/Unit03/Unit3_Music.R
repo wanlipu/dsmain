6c0a7b6175b14378290246fb9dc9a0547f94f7da
wanlipu <puwanli@gmail.com>
2016-05-08 23:40:42 -0500

    updates

--- theAnalyticsEdge/Unit03/Unit3_Music.R
+++ theAnalyticsEdge/Unit03/Unit3_Music.R
@@ -23,7 +23,7 @@ songs[which.max(songs$tempo),]
 songsTrain = subset(songs, songs$year <= 2009)
 songsTest = subset(songs, songs$year == 2010)
 
-# problem 2.2
+# problem 2.2 and 2.3 and 2.4 and 2.5
 
 # exclude non-numerical variables 
 nonvars = c("year", "songtitle", "artistname", "songID", "artistID")
@@ -32,3 +32,29 @@ songsTest = songsTest[ , !(names(songsTest) %in% nonvars) ]
 
 songsLog1 = glm(Top10 ~ ., data=songsTrain, family=binomial)
 summary(songsLog1)
+
+# problem 3.1
+cor(songsTrain[c("loudness", "energy")])
+
+# problem 3.2 and 3.3
+songsLog2 = glm(Top10 ~ . - loudness, data=songsTrain, family=binomial)
+summary(songsLog2)
+
+songsLog3 = glm(Top10 ~ . - energy, data=songsTrain, family=binomial)
+summary(songsLog3)
+
+# problem 4.1
+songsTestPred3 = predict(songsLog3, newdata = songsTest, type = "response")
+table(songsTest$Top10, songsTestPred3 >= 0.45)
+(309 + 19) / (373)
+
+# problem 4.2
+table(songsTest$Top10)
+314/373
+
+# problem 4.3
+# 19 and 5
+
+# problem 4.4 and 4.5
+19/(40+19)
+309/(309+5)
