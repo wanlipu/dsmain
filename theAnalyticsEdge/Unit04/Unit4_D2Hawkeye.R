11575d5466804aa418ca28578854fc928000767d
wanlipu <puwanli@gmail.com>
2016-05-13 15:46:28 -0500

    updates

--- theAnalyticsEdge/Unit04/Unit4_D2Hawkeye.R
+++ theAnalyticsEdge/Unit04/Unit4_D2Hawkeye.R
@@ -22,13 +22,17 @@ ClaimsTrain = subset(Claims, spl==TRUE)
 
 ClaimsTest = subset(Claims, spl==FALSE)
 
+mean(ClaimsTrain$age)
+summary(ClaimsTrain)
 
 # VIDEO 7
 
 # Baseline method
 table(ClaimsTest$bucket2009, ClaimsTest$bucket2008)
 
-(110138 + 10721 + 2774 + 1539 + 104)/nrow(ClaimsTest)
+(110138 + 10721 + 2774 + 1452 + 104)/nrow(ClaimsTest)
+(110138 + 7787 + 3427 + 201 + 174)/nrow(ClaimsTest)
+
 
 # Penalty Matrix
 PenaltyMatrix = matrix(c(0,1,2,3,4,2,0,1,2,3,4,2,0,1,2,6,4,2,0,1,8,6,4,2,0), byrow=TRUE, nrow=5)
@@ -41,6 +45,13 @@ as.matrix(table(ClaimsTest$bucket2009, ClaimsTest$bucket2008))*PenaltyMatrix
 sum(as.matrix(table(ClaimsTest$bucket2009, ClaimsTest$bucket2008))*PenaltyMatrix)/nrow(ClaimsTest)
 
 
+PenaltyMatrix2 = matrix(c(0, 2, 4, 6, 8), byrow=TRUE, nrow=5)
+PenaltyMatrix2
+table(ClaimsTest$bucket2009)
+
+#bucket2009 = matrix(c(122978, 34840, 16390, 7937, 1057), byrow=TRUE, nrow=5)
+sum(as.matrix(table(ClaimsTest$bucket2009))*PenaltyMatrix2)/nrow(ClaimsTest)
+
 # VIDEO 8
 
 # Load necessary libraries
@@ -48,6 +59,7 @@ library(rpart)
 library(rpart.plot)
 
 # CART model
+summary(ClaimsTrain)
 ClaimsTree = rpart(bucket2009 ~ age + alzheimers + arthritis + cancer + copd + depression + diabetes + heart.failure + ihd + kidney + osteoporosis + stroke + bucket2008 + reimbursement2008, data=ClaimsTrain, method="class", cp=0.00005)
 
 prp(ClaimsTree)
@@ -59,6 +71,7 @@ PredictTest = predict(ClaimsTree, newdata = ClaimsTest, type = "class")
 table(ClaimsTest$bucket2009, PredictTest)
 
 (114141 + 16102 + 118 + 201 + 0)/nrow(ClaimsTest)
+
 
 # Penalty Error
 as.matrix(table(ClaimsTest$bucket2009, PredictTest))*PenaltyMatrix
